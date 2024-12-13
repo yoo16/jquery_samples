cd2af541f1bb3736b05065762b147181116fcf01
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-13 23:22:26 +0900

    update

--- menu/js/dropdown.js
+++ menu/js/dropdown.js
@@ -1,6 +1,9 @@
 $(function () {
-    // id=nav をホバー
-    $("#nav > li").hover(
+    var headerHeight = 0;
+    const $menuLink = $('#nav a[href^="#"]');
+
+    // ホバーでサブメニュー
+    $("#nav li").hover(
         function () {
             $(this).find("ul").stop().slideDown(200);
         },
@@ -8,4 +11,18 @@ $(function () {
             $(this).find("ul").stop().slideUp(200);
         }
     );
+    $("#nav li").on('click', function () {
+        $(this).find("ul").stop().slideUp(200);
+    });
+
+    // スクロール固定
+    const navPos = $("#nav").offset().top;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > navPos) {
+            $("#nav").css("position", "fixed");
+        } else {
+            $("#nav").css("position", "static");
+        }
+    });
+
 });
