e5afce003a2b636febcdd33191d6c85aa52cd69c
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-13 22:26:59 +0900

    update

--- basic/js/image_caption.js
+++ basic/js/image_caption.js
@@ -6,10 +6,9 @@ $(function () {
         const listItem = `
             <li class="relative group">
                 <img src="${data.image}" class="w-full h-auto rounded-lg shadow-md">
-                <div
-                    class="item hidden absolute inset-0 bg-teal-600 rounded-lg p-3 bg-opacity-70 flex items-center justify-center text-white">
-                    <p class="caption p-4">${data.name}</p>
-                    <p class="caption p-4">${data.caption}</p>
+                <div class="caption hidden absolute inset-0 bg-teal-600 rounded-lg p-3 bg-opacity-85 flex items-center justify-center text-white">
+                    <div class="name p-4">${data.name}</div>
+                    <div class="caption p-4">${data.caption}</div>
                 </div>
             </li>
         `;
@@ -20,11 +19,11 @@ $(function () {
     $imageList.on({
         mouseenter: function () {
             // マウスオーバー時に説明文のdivを表示
-            $(this).find('.item').stop().fadeIn(200);
+            $(this).find('.caption').stop().fadeIn(200);
         },
         mouseleave: function () {
             // マウスアウト時に説明文のdivを非表示
-            $(this).find('.item').stop().fadeOut(200);
+            $(this).find('.caption').stop().fadeOut(200);
         }
     }, 'li');
 });
---
+++ menu/js/slide.js
@@ -0,0 +1,38 @@
+$(function () {
+    const $menuButton = $('#menu-button');
+    const $slideMenu = $('#slide-menu');
+    const headerHeight = $('#header-menu').outerHeight() + 10;
+
+    function initMenu() {
+        $slideMenu.css({
+            transform: 'translateX(-100%)',
+        });
+    }
+
+    function slideMenu() {
+        var x = '0px';
+        $slideMenu.toggleClass('on');
+        if ($slideMenu.hasClass('on')) {
+            x = $slideMenu.width() + 'px';
+        }
+        $slideMenu.animate({ marginLeft: x }, 300);
+    }
+
+    $menuButton.on('click', function () {
+        slideMenu();
+    });
+
+    $('#slide-menu a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        slideMenu();
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            const targetOffset = target.offset().top - headerHeight;
+            $('html, body').stop().animate({
+                scrollTop: targetOffset
+            }, 500);
+        }
+    });
+
+    initMenu();
+});
