1fd30abb5671cc9234c8a81fba739654ac853485
yoo16 <yohei.yoshikawa@gmail.com>
2025-01-08 15:26:03 +0900

    update

--- image_view/js/rolling.js
+++ image_view/js/rolling.js
@@ -1,20 +1,25 @@
+// jQuery animate()
 $(document).ready(function () {
     $('#next-button1').on('click', function () {
         const $images = $('#image-container').children('.stacked-item');
         const $topImage = $images.last();
 
+        // 現在の要素を取得して移動
         $topImage
             .css('z-index', 10)
             .animate({ opacity: 0.8, left: '100%', top: '-10x' }, 500,
+                // 移動終了後の処理
                 function () {
+                    // 重ね順を auto
                     $(this).css('z-index', 'auto');
+                    // 要素を先頭に移動し、フェードイン
                     $(this).prependTo('#image-container')
                         .animate({ opacity: 1, left: 0, top: 0 }, 500);
                 });
     });
 });
 
-
+// CSS animate
 $(document).ready(function () {
     $('#next-button2').on('click', function () {
         const $images = $('#image-container').children('.stacked-item');
@@ -23,6 +28,7 @@ $(document).ready(function () {
         // フェードアウト
         $topImage.addClass('animate-out');
 
+        // 移動終了後の処理
         $topImage.one('transitionend', function () {
             // 要素を先頭に移動
             $(this).prependTo('#image-container');
