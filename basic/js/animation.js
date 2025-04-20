05971c8f631bc10099a504a520a9f68211909da5
yoo16 <yohei.yoshikawa@gmail.com>
2025-04-20 14:20:05 +0900

    update

--- basic/js/animation.js
+++ basic/js/animation.js
@@ -1,49 +1,69 @@
 $(function () {
-    var panel = $("#panel");
+    var character_1 = $("#character_1");
+    var character_2 = $("#character_2");
+    var character_3 = $("#character_3");
+    var character_4 = $("#character_4");
     var box1 = $("#box1");
     var box2 = $("#box2");
 
     $("#showBtn").on("click", function () {
-        panel.show(500);
+        character_1.show(500);
     });
 
     $("#hideBtn").on("click", function () {
-        panel.hide(500);
+        character_1.hide(500);
     });
 
     $("#toggleBtn").on("click", function () {
-        panel.toggle(500);
+        character_1.toggle(500);
     });
 
     $("#fadeInBtn").on("click", function () {
-        panel.fadeIn(500);
+        character_2.fadeIn(500);
     });
 
     $("#fadeOutBtn").on("click", function () {
-        panel.fadeOut(500);
+        character_2.fadeOut(500);
     });
 
     $("#fadeToggleBtn").on("click", function () {
-        panel.fadeToggle(500);
+        character_2.fadeToggle(500);
     });
 
     $("#slideDownBtn").on("click", function () {
-        panel.slideDown(500);
+        character_3.slideDown(500);
     });
 
     $("#slideUpBtn").on("click", function () {
-        panel.slideUp(500);
+        character_3.slideUp(500);
     });
 
     $("#slideToggleBtn").on("click", function () {
-        panel.slideToggle(500);
+        character_3.slideToggle(500);
+    });
+
+    $("#shakeBtn").on("click", function () {
+        let distance = 10; // 揺れ幅
+        let times = 4;     // 揺れる回数
+
+        function shake(element, count) {
+            if (count > 0) {
+                console.log(element);
+                element.animate({ left: -distance }, 50)
+                    .animate({ left: distance }, 50, function () {
+                        shake(element, count - 1);
+                    });
+            } else {
+                element.animate({ left: 0 }, 50); // 最後に元の位置へ
+            }
+        }
+        shake(character_4, times);
     });
 
     $("#easingBtn").on("click", function () {
-        initBox();
-        box1.animate({ width: "300px" }, 1500, "linear");
-        box2.animate({ width: "300px" }, 1500, "swing");
-    })
+        box1.animate({ width: "300px" }, 1000, "linear");
+        box2.animate({ width: "300px" }, 1000, "swing");
+    });
 
     $("#animateBtn").on("click", function () {
         box1.animate({ width: "300px", height: "50px", opacity: 0.1 }, 1000)
@@ -62,7 +82,7 @@ $(function () {
         const delayTime = 300;
 
         items.each(function (i) {
-            $(this).delay((i + 1) * delayTime).queue(() => {
+            $(this).delay(i * delayTime).queue(() => {
                 $(this).removeClass('hidden').dequeue();
             });
         });
